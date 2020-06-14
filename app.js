502bdb47b2b25b333cb11bf8e802ef5a48ebab6c
Sheharyar-ans <62901867+Sheharyar-ans@users.noreply.github.com>
2020-06-14 17:38:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+
+// Chapter #1 Task # 1
+//alert("Welcome to Pakweb");
+
+//alert("Your password is Incorrect")
+
+//alert("Welcome To Js Land"<br>"Happy Coding");
+
+//var a = 5;
+//var b = 10;
+//var c = a+b;
+//alert(c)
+
+//var b = 10 % 3;
+//alert(b)
+
+//var x = 5;
+//var y = x++ + ++x + x++ - --x;
+//console.log(y);
+
+//var a = prompt("Enter Your Name","Your Name is:");
+//alert(a);
+
+// var a = prompt("Enter Name","Enter Your Nmae")
+// var b = prompt("Enter Father Nmae","Father Name")
+// var c = a + b
+// document.write(c);
+
+// var a = prompt("Enter Your Age","Your Age")
+// var b = prompt("Enter Your Height")
+// var c = a + b
+// document.write(c)
+
+// var x = "Enter value";
+// var y = "your Value is";
+// var a = +prompt(x,y);
+// var b = a + 10;
+// document.write("The Output is;" + " " + b)
+
+// var age = +prompt("Enter your age");
+// if(age == 18){
+//     alert("Ypur Age is Equal")
+// }
+// else if(age < 18){
+//     alert("Your Age is less")
+// }
+// else{
+//     alert("Your Age is More")
+// }
+
+// var a = "Sheharyar";
+// var b = "Ansari";
+// var c = a + " " + b
+// alert(c);
+
+// var a = 20;
+// var b = "20";
+// var c = a + b;
+// alert(c);
+
+// var a = +prompt("Enter Value");
+// var b = +prompt("Enter value");
+// var c = a + b;
+// document.write(c);
+
+// var a = prompt("Enter Your Average Marks");
+// var b = prompt("Enter Your Total Marks");
+// var c = a * 100 / b;
+// document.write(c);
+
+// var student = ["Sheharyar","Talha","Hadeed","Samir"];
+// document.write("Welcome Our Website" + " " +student[0]);
+
+
+// var names = [];
+// names[0] = "Sheharyar";
+// names[1] = "Samir";
+// names[2] = "Talha";
+// alert(names);
+
+// var student = ["Sheharyar","Talha","Hadeed","Samir"];
+// student.pop()
+// document.write(student);
+
+// var student = ["Sheharyar","Talha","Samir","Hadeed"];
+// student.push("Fahad");
+// console.log(student);
+
+// var student = ["Sheharyar","Talha","Samir","Hadeed"];
+// student.shift()
+// console.log(student);
+
+// var student = ["Sheharyar","Talha","Samir","Hadeed"];
+// student.unshift("Fahad");
+// console.log(student);
+
+// var student = ["Sheharyar","Talha","Samir","Hadeed"];
+// student.splice(1,0,"Fahad")
+// console.log(student);
+
+
+// var student = ["Sheharyar","Talha","Samir","Hadeed"];
+// student.splice(1,1)
+// console.log(student);
+
+// var student = ["Sheharyar","Talha","Samir","Hadeed","Fahad"];
+// var copystudent = student.slice(1,3)
+// console.log(copystudent);
+// console.log(student);
+
+
+
+// var a = prompt("Write Table Of Number");
+// var b = prompt("Write Length Of Table");
+// alert(a * b);
+
+
+// for(var i = 1; i <= 10; i++){
+//     document.write("2" + "X" + i + "=" + 2*i + "<br>")
+// }
+
+
+
+var a = prompt("Weite Table Number");
+for(var i = 1; i <= 10; i++){
+    document.write(a + "X" + i + "=" + a*i + "<br>")
+}
